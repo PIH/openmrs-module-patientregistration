4b84f5dd976971de42d4c4c2988ef1c49a2b5260
Mário <mariossareias@gmail.com>
2012-09-21 18:29:09 -0300

    Refactor test about generate ids inside EnterPatientDemoController

--- omod/src/test/java/org/openmrs/module/patientregistration/controller/workflow/EnterPatientDemoControllerTest.java
+++ omod/src/test/java/org/openmrs/module/patientregistration/controller/workflow/EnterPatientDemoControllerTest.java
@@ -3,8 +3,6 @@
  */
 package org.openmrs.module.patientregistration.controller.workflow;
 
-import java.util.Calendar;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.openmrs.Patient;
@@ -16,48 +14,60 @@ import org.springframework.ui.ExtendedModelMap;
 import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Date;
+
+import static org.openmrs.module.reporting.common.DateUtil.getDateTime;
+
 /**
  * @author cospih
  *
  */
 public class EnterPatientDemoControllerTest extends  BasePatientRegistrationControllerTest{
-	
-	@Test
+
+    public static final String GIVEN_NAME = "John";
+    public static final String FAMILY_NAME = "Riley";
+    public static final String GENDER = "M";
+    public static final String HIDDEN_PRINT_ID_CARD = "no";
+
+    @Test
 	public void processSelectPatient_shouldRegisterNewPatient() throws Exception{
-		
-		Patient patient = new Patient();
-		PersonName personName = new PersonName();
-		personName.setGivenName("John");
-		Assert.assertEquals("John", personName.getGivenName());
-		
-		personName.setFamilyName("Riley");
-		patient.addName(personName);
-		patient.getPersonName().setPreferred(true);
-		//set patient birthdate to March 22, 1984
-		Calendar birthdate = Calendar.getInstance();
-		birthdate.set(Calendar.YEAR, 1984);
-		birthdate.set(Calendar.MONTH, 2);
-		birthdate.set(Calendar.DAY_OF_MONTH, 22);
-		patient.setBirthdate(birthdate.getTime());
-		patient.setGender("M");
-		
-		EnterPatientDemoController controller = new EnterPatientDemoController();
-		Birthdate patientBirthdate = new Birthdate(birthdate.getTime());
-		ExtendedModelMap model = new ExtendedModelMap();
-		BindException result = new BindException(patient, "patient");
+
+        Date birthdate = getDateTime(1984, 2, 22);
+
+        Patient patient = new Patient();
+        patient.addName(buildPatientName(GIVEN_NAME, FAMILY_NAME));
+        patient.getPersonName().setPreferred(true);
+        patient.setBirthdate(birthdate);
+		patient.setGender(GENDER);
+
+        Birthdate patientBirthdate = new Birthdate(birthdate);
+
+        EnterPatientDemoController controller = new EnterPatientDemoController();
+
+        BindException result = new BindException(patient, "patient");
 		BindException birthdateResult = new BindException(patientBirthdate, "birthdate");
 		BindException ageResult = new BindException(new Age(), "age");
-		ModelAndView modelAndView =controller.processSelectPatient(patient, result, patientBirthdate, birthdateResult, null, ageResult, 
-				"John", "Riley", "M", "2nd ave,Cange,3me La Hoye,Lascahobas,Centre,Haiti", "7865-0998", 
-				null, "no", session, model);
+
+        String patientAddress = "2nd ave,Cange,3me La Hoye,Lascahobas,Centre,Haiti";
+        String phoneNumber = "7865-0998";
+        ModelAndView modelAndView =controller.processSelectPatient(patient, result, patientBirthdate, birthdateResult, null, ageResult,
+				GIVEN_NAME, FAMILY_NAME, GENDER, patientAddress, phoneNumber,
+				null, HIDDEN_PRINT_ID_CARD, session, new ExtendedModelMap());
 			
 		String viewName = modelAndView.getViewName();
-		Assert.assertEquals("redirect:/module/patientregistration/workflow/patientDashboard.form?patientId="+patient.getId(), viewName);
-		log.debug("viewName="+viewName);
-		Patient newPatient = Context.getPatientService().getPatient(patient.getId());
+		Assert.assertEquals("redirect:/module/patientregistration/workflow/patientDashboard.form?patientId=" + patient.getId(), viewName);
+
+        Patient newPatient = Context.getPatientService().getPatient(patient.getId());
 		Assert.assertNotNull(newPatient);
-		Assert.assertEquals("Riley", newPatient.getFamilyName());
+		Assert.assertEquals(FAMILY_NAME, newPatient.getFamilyName());
 		
 		
 	}
+
+    private PersonName buildPatientName(String givenName, String familyName) {
+        PersonName patientName = new PersonName();
+        patientName.setGivenName(givenName);
+        patientName.setFamilyName(familyName);
+        return patientName;
+    }
 }
