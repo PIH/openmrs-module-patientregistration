30c4e2ac0d2347c264025b5cb47151b959e7f1b3
Mário <mario.s.s.areias@gmail.com>
2012-12-07 18:39:42 -0200

    #25 creating test to ensure that the printer service is being called

---
+++ omod/src/test/java/org/openmrs/module/patientregistration/controller/workflow/PatientRegistrationDashboardControllerTest.java
@@ -0,0 +1,17 @@
+package org.openmrs.module.patientregistration.controller.workflow;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mario
+ * Date: 12/7/12
+ * Time: 4:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PatientRegistrationDashboardControllerTest {
+    @Test
+    public void testHandleCardPrinting() throws Exception {
+
+    }
+}
